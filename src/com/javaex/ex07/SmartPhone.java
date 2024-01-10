302725a80e047910f0e888d0e4b380a6cdb84bfc
공주 <@172.30.1.97>
2024-01-11 00:40:59 +0900

    01-11

--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -1,32 +1,26 @@
 package com.javaex.ex07;
 
 public class SmartPhone extends MusicPhone {
-    
-  
-        
-        //코드작성
-    
 
-    	// 메소드
+	public void execute(String str) {
 
-    	  public void execute(String str){
-    		if (str.equals("앱")) {
-    			System.out.println("앱실행");
-    		} else if (str.equals("음악")) {
-    			System.out.println("다운로드해서 음악재생");
-    		} else {
-    			System.out.println("통화기능시작");
-    		}
+		// 코드작성
+		if (str.equals("앱")) {
+			execute();
+		} else if (str.equals("음악")) {
+			playMusic();
+		} else if (str.equals("통화")) {
+			super.execute(str);
+		}
+	}
 
-    	}
+	// 메소드작성
+	public void execute() {
+		System.out.println("앱실행");
+	}
 
-  
-    }
- 
-    //메소드작성
-    
-    //메소드작성
-    
-    
-    
-
+	// 메소드작성
+	public void playMusic() {
+		System.out.println("다운로드해서 음악실행");
+	}
+}
